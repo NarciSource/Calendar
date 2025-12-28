dbf45fe6d329cec4fc91b01cb2e459f29b28f669
NarciSource <jwch11@gmail.com>
2025-12-28 18:15:30 +0900

    refactor: Jwt 인터셉터/서비스 에러 처리

--- src/main/kotlin/com/pickme/calendar/adapter/inbound/web/interceptor/JwtInterceptor.kt
+++ src/main/kotlin/com/pickme/calendar/adapter/inbound/web/interceptor/JwtInterceptor.kt
@@ -1,8 +1,11 @@
 package com.pickme.calendar.adapter.inbound.web.interceptor
 
 import com.pickme.calendar.adapter.inbound.web.security.JwtService
+import com.pickme.calendar.application.exception.CustomException
+import com.pickme.calendar.application.exception.ErrorCode
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springdoc.core.utils.Constants.OPTIONS_METHOD
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
 
@@ -13,28 +16,19 @@ class JwtInterceptor(val jwtService: JwtService) : HandlerInterceptor {
         request: HttpServletRequest,
         response: HttpServletResponse,
         handler: Any
-    ): Boolean {
+    ): Boolean = when (request.method.uppercase()) {
+        OPTIONS_METHOD -> true
 
-        // OPTIONS 통과
-        if ("OPTIONS".equals(request.method, ignoreCase = true)) {
-            return true
+        else -> {
+            // 클라이언트 요청 헤더에서 Authorization 정보를 가져옴
+            request.getHeader("Authorization")
+                ?.takeIf { it.startsWith("Bearer ") }
+                ?.removePrefix("Bearer ")
+                ?.trim()
+                ?.let(jwtService::extractToken)
+                ?.also { request.setAttribute("clientId", it) }
+                ?: throw CustomException(ErrorCode.INVALID_AUTH_TOKEN)
+            true
         }
-
-        // 클라이언트 요청 헤더에서 Authorization 정보를 가져옴
-        val token = request.getHeader("Authorization")
-        // Authorization 헤더가 없거나 Bearer 형식이 아니면 401(Unauthorized) 상태 코드를 반환하고 요청을 중단
-        if (token == null || !token.startsWith("Bearer ")) {
-            response.status = HttpServletResponse.SC_UNAUTHORIZED // 응답 상태를 401로 설정
-            return false // 요청 처리 중단
-        }
-
-        // 토큰에서 사용자 정보를 추출
-        val clientId = jwtService.extractToken(token)
-
-        // HttpServletRequest에 사용자 정보를 속성으로 추가하여 컨트롤러에서 사용할 수 있게 함
-        request.setAttribute("clientId", clientId)
-
-        // 요청 처리를 계속 진행
-        return true
     }
 }
--- src/main/kotlin/com/pickme/calendar/adapter/inbound/web/security/JwtService.kt
+++ src/main/kotlin/com/pickme/calendar/adapter/inbound/web/security/JwtService.kt
@@ -1,19 +1,23 @@
 package com.pickme.calendar.adapter.inbound.web.security
 
 import com.auth0.jwt.JWT
+import com.auth0.jwt.exceptions.JWTDecodeException
+import com.pickme.calendar.application.exception.CustomException
+import com.pickme.calendar.application.exception.ErrorCode
 import org.springframework.stereotype.Service
 
 @Service
 class JwtService {
     // JWT토큰 처리하는 로직
-    fun extractToken(bearerToken: String): String? {
-        // "Bearer " 접두사 제거
-        val token = bearerToken.replace("Bearer ", "")
-
-        // JWT 토큰을 디코딩
-        val decodedJwt = JWT.decode(token)
-
-        // "client_id" 클레임을 문자열로 추출하여 반환
-        return decodedJwt.getClaim("client_id").asString()
+    fun extractToken(token: String): String {
+        return try {
+            // JWT 토큰을 디코딩
+            val decodedJwt = JWT.decode(token)
+            // "client_id" 클레임을 문자열로 추출하여 반환
+            decodedJwt.getClaim("client_id").asString()
+                ?: throw CustomException(ErrorCode.INVALID_AUTH_TOKEN)
+        } catch (_: JWTDecodeException) {
+            throw CustomException(ErrorCode.INVALID_AUTH_TOKEN)
+        }
     }
 }
