6f79c5bdb6d6c77f709d133ec67bdb58e2ada2ce
NarciSource <jwch11@gmail.com>
2025-12-30 22:38:11 +0900

    security: Jwt 기반 Spring Security 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,6 +72,8 @@ dependencies {
     // Spring Boot
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
---
+++ src/main/kotlin/com/pickme/calendar/infrastructure/config/SecurityConfig.kt
@@ -0,0 +1,52 @@
+package com.pickme.calendar.infrastructure.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() } // CSRF 비활성화, CSRF는 브라우저 + 쿠키 + 세션 조합에서 사용
+            .authorizeHttpRequests { auth ->
+                auth
+                    // Swagger
+                    .requestMatchers(
+                        "/swagger-ui/**",
+                        "/v3/api-docs**",
+                        "/v3/api-docs/**",
+                    ).permitAll()
+
+                    // Health Check
+                    .requestMatchers(
+                        "/",
+                    ).permitAll()
+
+                    // Internal API
+                    .requestMatchers("/internal/**")
+                    .hasAuthority("SCOPE_INTERNAL")
+
+                    // External API
+                    .requestMatchers("/api/**")
+                    .authenticated()
+
+                    // Default
+                    .anyRequest().denyAll()
+            }
+            .oauth2ResourceServer {
+                it.jwt {} // JWT 검증. JwtDecoder 자동 감지
+            }
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 생성 거부
+            }
+
+        return http.build()
+    }
+}
