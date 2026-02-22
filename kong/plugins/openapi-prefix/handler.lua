9520166c942033450eebc1b97c58673c133cf321
NarciSource <jwch11@gmail.com>
2026-02-22 13:59:42 +0900

    fix(kong): openapi-prefix 플러그인 chunk 문제 수정
    - chunk 버퍼링
    - body length 자동 계산을 위한 Content-Length 제거

--- kong/plugins/openapi-prefix/handler.lua
+++ kong/plugins/openapi-prefix/handler.lua
@@ -6,18 +6,35 @@ local OpenApiPrefix = {
 -- Lua JSON
 local cjson = require("cjson.safe")
 
-function OpenApiPrefix:body_filter(conf)
-	local body = kong.response.get_raw_body()
+function OpenApiPrefix:header_filter(conf)
+	kong.response.clear_header("Content-Length") -- body length 자동 계산을 위해 삭제
+end
 
-	-- api 문서 수정
-	local json = cjson.decode(body)
-	if json.servers and #json.servers > 0 then
-		json.servers[1].url = conf.prefix
-	else
-		json.servers = { { url = conf.prefix } }
+function OpenApiPrefix:body_filter(conf)
+	-- chunk 처리
+	local chunk = ngx.arg[1] -- 현재 body chunk
+	local eof = ngx.arg[2] -- 마지막 chunk 여부
+
+	ngx.ctx.buffered = (ngx.ctx.buffered or "") .. (chunk or "") -- chunk를 버퍼링
+
+	if not eof then
+		ngx.arg[1] = nil
+		return
 	end
 
-	body = cjson.encode(json)
+	local body = ngx.ctx.buffered
+
+	-- json 수정
+	local json = cjson.decode(body)
+	if json and (json.openapi or json.swagger) then
+		if json.servers and #json.servers > 0 then
+			json.servers[1].url = conf.prefix
+		else
+			json.servers = { { url = conf.prefix } }
+		end
+
+		body = cjson.encode(json)
+	end
 
 	ngx.arg[1] = body
 end
